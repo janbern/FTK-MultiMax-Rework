b081783b32e19b0999a1ed53d0bdef03f2d55cb6
justedm <75849232+justedm@users.noreply.github.com>
2024-01-17 21:21:38 +0100

    Fixed gold menu position UI error

--- uiGoldMenuPatches.cs
+++ uiGoldMenuPatches.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using UnityEngine;
 
@@ -10,12 +11,16 @@ namespace FTK_MultiMax_Rework {
         public static bool GoldAwake(uiGoldMenu __instance) {
             var m_GoldEntriesField = Traverse.Create(__instance).Field("m_GoldEntries");
 
+            __instance.m_InputFocus = __instance.gameObject.GetComponent<FTKInputFocus>();
+            __instance.m_InputFocus.m_InputMode = FTKInput.InputMode.InGameUI;
+            __instance.m_InputFocus.m_Cancel = __instance.OnButtonCancel;
+
             if (m_GoldEntriesField.GetValue() != null) {
-                int maxEntries = (GameFlowMC.gMaxPlayers - 1);
+                int maxEntries = (GameFlowMC.gMaxPlayers - 2);
 
                 m_GoldEntriesField.GetValue<List<uiGoldMenuEntry>>().Add(__instance.m_FirstEntry);
 
-                for (int i = 1; i < maxEntries; i++) {
+                for (int i = 0; i < maxEntries; i++) {
                     uiGoldMenuEntry newEntry = UnityEngine.Object.Instantiate(__instance.m_FirstEntry);
                     newEntry.transform.SetParent(__instance.m_FirstEntry.transform.parent, worldPositionStays: false);
                     m_GoldEntriesField.GetValue<List<uiGoldMenuEntry>>().Add(newEntry);
